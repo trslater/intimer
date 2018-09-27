fcb969af20e7b53e7ef44f59f8d7d1c6a4d317ed
trslater <hello@trslater.ca>
2018-09-27 10:38:29 -0700

    Add cycling timers

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-import time
+from time import time, sleep
+from itertools import cycle
 
-while True:
-    start = time.time()
-    print('.')
-    time.sleep(1 - (time.time() - start))
+timers = cycle([
+    {
+        "duration":     1200,
+        "done message": "Time to rest your eyes!"
+    },
+    {
+        "duration":     20,
+        "done message": "Back to work..."
+    },
+])
+
+def main():
+    for timer in timers:
+        for remaining in range(timer["duration"]):
+            start = time()
+
+            print(remaining)
+
+            sleep(1 - (time() - start))
+
+def get_next_timer():
+    for timer in timers:
+        yield timer
+
+if __name__ == "__main__":
+    main()
